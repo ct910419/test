62a9e4bdd39c0ba994e4e7f31ba274075a591c2a
ct910419 <ctct910419@gmail.com>
2020-12-24 19:35:19 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,174 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="bg">
+      <img src="https://i.ibb.co/fDTcBnG/20171225193112-2144.jpg" alt="20171225193112-2144" border="0">
+    </div>
+    <div id="a">
+      <img src="https://i.ibb.co/Fh8m87t/image-1.png" alt="image-1" border="0">
+    </div>
+    <div id="bg2">
+      <img src="https://i.ibb.co/3s7Znh2/20171225193112-2145.jpg" alt="20171225193112-2145" border="0">
     </div>
     <router-view/>
+    <ul class="drop-down-menu">
+        <li><a href="#">。最新消息</a>
+        </li>
+        <li><a href="#">。招生</a>
+          <ul>
+            <li><a href="#">春之清華卓越獎學金</a>
+            </li>
+            <li><a href="#">招生Q&A</a>
+            </li>
+            <li><a href="#">招生資料</a>
+            <ul>
+              <li><a href="#">2020</a>
+              <ul>
+                <li><a href="#">錄取名單</a>
+                </li>
+                <li><a href="#">招生簡章</a>
+                </li>
+                <li><a href="#">報名資料繳交</a>
+                </li>
+                <li><a href="#">第二階段注意事項</a>
+                </li>
+              </ul>
+              </li>
+              <li><a href="#">2019</a>
+              <ul>
+                <li><a href="#">錄取名單</a>
+                </li>
+                <li><a href="#">招生簡章</a>
+                </li>
+                <li><a href="#">報名資料繳交</a>
+                </li>
+                <li><a href="#">第二階段注意事項</a>
+                </li>
+              </ul>
+              </li>
+              <li><a href="#">2018</a>
+              <ul>
+                <li><a href="#">錄取名單</a>
+                </li>
+                <li><a href="#">招生簡章</a>
+                </li>
+                <li><a href="#">報名資料繳交</a>
+                </li>
+                <li><a href="#">第二階段注意事項</a>
+                </li>
+              </ul>
+              </li>
+            </ul>
+            </li>
+          </ul>
+        </li>
+        <li><a href="#">。學士班</a>
+        <ul>
+          <li><a href="#">學士班FB粉絲專頁</a>
+          </li>
+          <li><a href="#">學士班簡介</a>
+          </li>
+          <li><a href="#">學士班課程</a>
+          </li>
+          <li><a href="#">國際交換</a>
+          </li>
+          <li><a href="#">藝術學院大樓</a>
+          </li>
+        </ul>
+        </li>
+        <li><a href="#">。師資</a>
+        </li>
+        <li><a href="#">。科藝賞析</a>
+        <ul>
+          <li><a href="#">數位文創作品</a>
+          </li>
+          <li><a href="#">互動藝術作品</a>
+          </li>
+          <li><a href="#">動力藝術作品</a>
+          </li>
+          <li><a href="#">聲音藝術作品</a>
+          </li>
+          <li><a href="#">科技表演藝術</a>
+          </li>
+        </ul>
+        </li>
+        <li><a href="#">。聯絡</a>
+        </li>
+    </ul>
   </div>
 </template>
 
 <style>
+ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  ul.drop-down-menu {
+    position: fixed;
+    left: 532px;
+    top: 117px;
+    color: #1B263B;
+    font-family: 'ABeeZee';
+    font-size: 24px;
+    z-index: 999;
+  }
+
+#bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -999;
+}
+
+#bg img{
+  min-height: 100%;
+  width: 100%;
+  opacity: 100%;
+}
+
+#a{
+  position: fixed;
+  top: 108px;
+  left: 380px;
+  z-index: 999;
+}
+
+#a img{
+  width: 81px;
+  height: 86px;
+}
+
+#bg2{
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+#bg2 img{
+  height: 260px;
+  width: 100%;
+  opacity: 100%;
+}
+
+#b{
+  position: fixed;
+  top: 119px;
+  left: 482px;
+  z-index: 999;
+}
+
+#b img{
+  width: 1128px;
+  height: 61px;
+}
+
 #app {
+  position: absolute;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -17,16 +176,80 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
+ul {
+  margin: 1px;
+  padding: 0;
+  list-style: none;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  ul.drop-down-menu {
+    border: rgba(204, 204, 204, 0) 1px solid;
+    display: inline-block;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 24px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    ul.drop-down-menu li {
+        position: relative;
+        white-space: nowrap;
+        border-right: rgba(204, 204, 204, 0) 1px solid;
+    }
+
+    ul.drop-down-menu > li:last-child {
+        border-right: none;
+    }
+
+    ul.drop-down-menu > li {
+        float: left; /* 只有第一層是靠左對齊*/
+    }
+
+    ul.drop-down-menu a {
+        background-color: #e0e1dde0;
+        color: #1B263B;
+        display: block;
+        padding: 8px 40px;
+        text-decoration: none;
+        line-height: 40px;
+    }
+
+    ul.drop-down-menu a:hover { /* 滑鼠滑入按鈕變色*/
+        background-color: #1b263bd5;
+        color: #fff;
+    }
+
+    ul.drop-down-menu li:hover > a { /* 滑鼠移入次選單上層按鈕保持變色*/
+        background-color: #1b263bd5;
+        color: #fff;
+    }
+
+    ul.drop-down-menu ul {
+        border: #ccc 1px solid;
+        position: absolute;
+        z-index: 99;
+        left: -1px;
+        top: 100%;
+        min-width: 180px;
+    }
+
+    ul.drop-down-menu ul li {
+        border-bottom: #ccc 1px solid;
+    }
+
+    ul.drop-down-menu ul li:last-child {
+        border-bottom: none;
+    }
+
+    ul.drop-down-menu ul ul { /*第三層以後的選單出現位置與第二層不同*/
+        z-index: 999;
+        top: 0px;
+        left: 100%;
+    }
+
+      ul.drop-down-menu ul { /*隱藏次選單*/
+        display: none;
+    }
+
+    ul.drop-down-menu li:hover > ul { /* 滑鼠滑入展開次選單*/
+        display: block;
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,196 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="fr">
+  <div id="cS">
+    <p>國立清華大學藝術學院學士班</p>
   </div>
+  <div id="c">
+    <p>國立清華大學藝術學院學士班</p>
+  </div>
+  <div id="rec1">
+    <div id="ipta">
+      <img src="https://i.ibb.co/wLRF04Q/IPTA-1.png" alt="IPTA-1" border="0">
+    </div>
+    <div id="cc">
+      <p>學士班特色</p>
+      <div id="ccc">
+        <p>
+          清華是國際頂尖科技大學，加上合校後的藝術學院，具備發展「科技藝術」的卓越條件。我們號召臺灣年輕一代的科技藝術學者專家，我們規劃有科技藝術跨域特別課程，並與業界合作推出創意跨域專題。你，不需猶豫，歡迎你的加入！
+        </p>
+      </div>
+      <p>核心能力</p>
+      <div id="ccc">
+        社會關懷與品格實踐能力｜美感認知與經典欣賞能力｜自然感知與創新發想能力<br />邏輯思辨與語文溝通能力｜國際觀點與在地行動能力｜資訊掌握與科技應用能力
+      </div>
+      <div id="cccc">
+        清華大學2018年成立藝術學院〝科技藝術〞跨域「學士班」。「科技藝術（TechArt）」是當代藝術的新形式創作也是國際藝術發展的重要趨勢，包含虛擬藝術、互動藝術、電腦動畫、電腦音樂、聲音藝術、科技性表演藝術、混合藝術、網路社群、創客藝術、仿生藝術、生物藝術、人工生命…等。在當代數位美學與數位藝術的基礎下，我們結合創新科技，如各種互動感測技術、虛擬實境、物聯網、人工智慧、大數據分析、基因工程…，把「科技」與「藝術」結合視為「未來文化學」，培養前瞻性、跨域研究與創作人才。因應未來產業的發展趨勢，創新科技與當代藝術結合之創作如雨後春筍。清華大學是國內科學與科技的頂尖學府，對藝術學院發展「科技藝術」具有得天獨厚的優勢。本班師資除來自清華校內藝術學院、電資學院、工學院、生命科學院、人文社會等跨域教師外，我們也將聘進一群來自美國、歐洲與日本跨國與跨域的專業教師。也廣邀在臺灣在科技藝術創作領域最頂尖的業界團隊與藝術家加入我們的教學團隊，讓同學得到專業與多元的指導，並在共享學習、共享創意的環境裡，開創未來的產業，盡情發揮自己的潛能。
+      </div>
+    </div>
+    <div id="news">
+      <img src="https://i.ibb.co/VwQwTsZ/NEWS.png" alt="NEWS" border="0">
+    </div>
+    <div id="p">
+      <img src="https://i.ibb.co/K92XkHp/image-5.png" alt="image-5" border="0">
+    </div>
+    <div id="pp">
+      <img src="https://i.ibb.co/LdPSm8D/image-6.png" alt="image-6" border="0">
+    </div>
+    <div id="ppp">
+      <img src="https://i.ibb.co/7NDfyvn/image-7.png" alt="image-7" border="0">
+    </div>
+    <div id="char">
+      【獲獎】《那顆Ｇ1咻！》國立清華大學藝術學院學士班第一屆系展 得獎名單
+    </div>
+    <div id="charr">
+      清華大學【藝術學院大樓】建築比案終於出爐！
+    </div>
+    <div id="charrr">
+      【獲獎】2020春之清華 藝術卓越獎 科技藝術類 得獎名單
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style>
+#fr{
+  position: relative;
+  z-index: -100;
+}
+
+#c{
+  position: fixed;
+  top: 227px;
+  left: 660px;
+  font-size: 48px;
+  color: rgb(255, 255, 255);
+  text-align: center;
+  z-index: -1;
+}
+
+#cS{
+  position: fixed;
+  top: 229px;
+  left: 662px;
+  font-size: 48px;
+  color: rgba(56, 54, 54, 0.658);
+  text-align: center;
+  z-index: -2;
+}
+
+#rec1{
+  position: relative;
+  top: 417px;
+  left: 344px;
+  width: 1260px;
+  height: 1885px;
+  background: #E5E5E5;
+}
+
+#ipta{
+  position: relative;
+  top: 50px;
+}
+
+#cc{
+  position: relative;
+  top: 121px;
+  font-size: 24px;
+  color: #415A77;
+  text-align: center;
+  font-family: 'Alatsi';
+}
+
+#ccc{
+  position: relative;
+  color: #1B263B;
+  font-size: 18px;
+  width: 780px;
+  height: 150px;
+  letter-spacing: 50%;
+  line-height: 50px;
+  margin: auto;
+}
+
+#cccc{
+  position: relative;
+  color: #1b263b65;
+  font-size: 14px;
+  width: 940px;
+  height: 501px;
+  letter-spacing: 50%;
+  line-height: 50px;
+  margin: auto;
+  text-align:justify;
+}
+
+#news{
+  position: relative;
+  top: 50px;
+}
+
+#p{
+  position: absolute;
+  top: 1300px;
+  left: 132px;
+}
+
+#pp{
+  position: absolute;
+  top: 1300px;
+  left: 502px;
+}
+
+#ppp{
+  position: absolute;
+  top: 1300px;
+  left: 872px;
+}
+
+#char{
+  position: absolute;
+  top: 1592px;
+  left: 132px;
+  font-size: 18px;
+  color: #415A77;
+  text-align: center;
+  font-family: 'Alatsi';
+  width: 273px;
+  height: 60px;
+  letter-spacing: 50%;
+  line-height: 40px;
+  margin: auto;
+}
+
+#charr{
+  position: absolute;
+  top: 1592px;
+  left: 502px;
+  font-size: 18px;
+  color: #415A77;
+  text-align: center;
+  font-family: 'Alatsi';
+  width: 273px;
+  height: 60px;
+  letter-spacing: 50%;
+  line-height: 40px;
+  margin: auto;
+}
+
+#charrr{
+  position: absolute;
+  top: 1592px;
+  left: 865px;
+  font-size: 18px;
+  color: #415A77;
+  text-align: center;
+  font-family: 'Alatsi';
+  width: 279px;
+  height: 60px;
+  letter-spacing: 50%;
+  line-height: 40px;
+  margin: auto;
+}
+
+</style>
